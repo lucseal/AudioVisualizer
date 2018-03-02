1363250b9abd5ada9d866960e896b3b1714da43e
sunyao <yao@lexislive.com.cn>
2018-03-02 18:11:21 +0800

    update

--- app/src/main/java/com/example/sunyao/audiovisualizer/App.java
+++ app/src/main/java/com/example/sunyao/audiovisualizer/App.java
@@ -12,7 +12,7 @@ import com.iflytek.cloud.SpeechUtility;
 public class App extends Application {
     // Branch X
     // Branch X
-    // Branch Z
+    // Branch Z + Z
 
     @Override
     public void onCreate() {
